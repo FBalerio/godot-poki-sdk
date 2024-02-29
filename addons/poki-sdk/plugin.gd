cf80777ec9affa2e36ca43baddbc1daeb83b3867
Franco Balerio <franco.balerio@gmail.com>
2024-02-29 12:04:13 -0300

    Porting plugin to 4.3
    On plugin:
    * updated tool line
    * added config for no threading
    
    On SDK handler:
    * added static typings to js objects to improve memory usage
    * updated function names to use snake case in accordance to GDScript's style guides
    * ported JavaScript class usages to JavaScriptObject and JavaScriptBridge
    * updated signal firing to modern methods
    * updated flag checking from javascript to web

--- addons/poki-sdk/plugin.gd
+++ addons/poki-sdk/plugin.gd
@@ -1,26 +1,25 @@
-tool
+@tool
 extends EditorPlugin
 
-
-func is_poki_added(cfg:ConfigFile):
+func is_poki_added(cfg: ConfigFile):
 	var pokiexists = false
 	for key in cfg.get_sections():
 		if cfg.has_section_key(key, "name"):
-			var name:String = cfg.get_value(key, "name")
+			var name: String = cfg.get_value(key, "name")
 			if name.begins_with("Poki"):
 				pokiexists = true
 				break
 	return pokiexists
 	
-func add_poki_export(cfg:ConfigFile):
+func add_poki_export(cfg: ConfigFile):
 	var arr = cfg.get_sections()
-	var num_exports = len(arr)/2
+	var num_exports = len(arr) / 2
 	var poki_section = "preset" + "." + str(num_exports)
 	var poki_options = poki_section + ".options"
 	
 	#preset.x
 	cfg.set_value(poki_section, "name", "Poki")
-	cfg.set_value(poki_section, "platform", "HTML5")
+	cfg.set_value(poki_section, "platform", "Web")
 	cfg.set_value(poki_section, "runnable", false)
 	cfg.set_value(poki_section, "custom_features", "")
 	cfg.set_value(poki_section, "export_filter", "all_resources")
@@ -28,19 +27,19 @@ func add_poki_export(cfg:ConfigFile):
 	cfg.set_value(poki_section, "exclude_filter", "")
 
 	#preset.x.options
+	cfg.set_value(poki_options, "thread_support", false)
 	cfg.set_value(poki_options, "html/custom_html_shell", "res://addons/poki-sdk/full-size.html")
 	cfg.set_value(poki_options, "html/head_include", """
-	<script> 
+	<script>
 		//place sitelock code here.
 	</script>
 	""")
-	
 	
 func _enter_tree():
 	var cfg = ConfigFile.new()
 	cfg.load("res://export_presets.cfg")
 
-	if(self.is_poki_added(cfg)):
+	if (self.is_poki_added(cfg)):
 		print("Poki export already added")
 	else:
 		add_poki_export(cfg)
@@ -48,7 +47,6 @@ func _enter_tree():
 	
 	add_autoload_singleton("PokiSDK", "res://addons/poki-sdk/pokisdk.gd")
 	pass
-
 
 func _exit_tree():
 	remove_autoload_singleton("PokiSDK")
--- addons/poki-sdk/pokisdk.gd
+++ addons/poki-sdk/pokisdk.gd
@@ -1,69 +1,74 @@
 extends Node
 
-var sdk_handle = null
-var _cb_commercial_break 
-var _cb_reward_break
-var _cb_shareable_url
+var sdk_handle: JavaScriptObject
+var _cb_commercial_break: JavaScriptObject
+var _cb_reward_break: JavaScriptObject
+var _cb_shareable_url: JavaScriptObject
 
-signal commercial_break_done(response) 
+signal commercial_break_done(response)
 signal rewarded_break_done(response)
 signal shareable_url_ready(url)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if OS.has_feature("JavaScript") == false:
+	if OS.has_feature("web") == false:
+		print("No JavaScript singleton detected")
 		return
 		
-	sdk_handle = JavaScript.get_interface("PokiSDK")
-	_cb_commercial_break = JavaScript.create_callback(self, "on_commercial_break")
-	_cb_reward_break = JavaScript.create_callback(self, "on_reward_break")
-	_cb_shareable_url = JavaScript.create_callback(self, "on_shareable_url")
+	sdk_handle = JavaScriptBridge.get_interface("PokiSDK")
+	_cb_commercial_break = JavaScriptBridge.create_callback(on_commercial_break)
+	_cb_reward_break = JavaScriptBridge.create_callback(on_reward_break)
+	_cb_shareable_url = JavaScriptBridge.create_callback(on_shareable_url)
 
-func gameplayStart():
+func gameplay_start():
 	if not sdk_handle:
+		print("Poki SDK Not Detected")
 		return
 	sdk_handle.gameplayStart()
 	
-func gameplayStop():
+func gameplay_stop():
 	if not sdk_handle:
+		print("Poki SDK Not Detected")
 		return
 	sdk_handle.gameplayStop()
 	
-func commercialBreak():
+func commercial_break():
 	if not sdk_handle:
+		print("Poki SDK Not Detected")
 		return
 	sdk_handle.commercialBreak().then(_cb_commercial_break)
 	
 func on_commercial_break(args):
 	print("Commercial break done!")
-	emit_signal("commercial_break_done", args[0])
+	commercial_break_done.emit(args[0])
 
-func rewardedBreak():
+func rewarded_break():
 	if not sdk_handle:
+		print("Poki SDK Not Detected")
 		return
 	sdk_handle.rewardedBreak().then(_cb_reward_break)
 	
 func on_reward_break(args):
 	print("Reward break done!")
-	emit_signal("rewarded_break_done", args[0])
+	rewarded_break_done.emit(args[0])
 	
-func shareableURL(obj:Dictionary):
+func shareable_url(obj: Dictionary):
 	if not sdk_handle:
 		return
-	var params = JavaScript.create_object("Object")
+	var params = JavaScriptBridge.create_object("Object")
 	
 	for key in obj.keys():
 		params[key] = obj[key]
 	sdk_handle.shareableURL(params).then(_cb_shareable_url)
 
 func on_shareable_url(url):
-	emit_signal("shareable_url_ready", url[0])
+	shareable_url_ready.emit(url[0])
 	
-func isAdBlocked():
+func is_ad_blocked():
 	var ret = sdk_handle.isAdBlocked()
 	return ret
 	
-func enableEventTracking():
+func enable_event_tracking():
 	if not sdk_handle:
 		return
 	sdk_handle.enableEventTracking()
